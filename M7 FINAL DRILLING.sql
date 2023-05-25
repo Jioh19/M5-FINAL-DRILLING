7097c5b02112b72a37ed04c06124b7a1077c8404
Jioh19 <jioh1982@gmail.com>
2023-05-25 18:23:01 -0400

    Entrega Final

--- M7 FINAL DRILLING.sql
+++ M7 FINAL DRILLING.sql
@@ -1,42 +1,75 @@
 -- • Aquellas usadas para insertar, modificar y eliminar un Customer, Staff y Actor.
 --* Insertar no tiene problemas. Eliminar o DELETE en cambio necesitan agregar 
---* "ON DELETE CASCADE" y UPDATE también a veces
+--* "ON DELETE CASCADE" y UPDATE también a veces. Lo había tratado de hacer asi, pero
+--* saber modificar tablas creo que no tenían mucho beneficio académico.... Quedó como
+--* recuerdo en el GITHUB.
+
+--* Lo hice todos los queries como ejemplos.
 
 --* TABLA CUSTOMER
+
+-- Insertar
 INSERT INTO customer 
-VALUES
-    (customer_id, store_id, first_name, last_name, email, address_id, activebool, create_date, last_update, active);
+VALUES 
+	(601, 1, 'Juan', 'Oh', 'correo1', 601, true, CURRENT_DATE, CURRENT_DATE, 1);
 
+-- Update	
 UPDATE customer
-SET 
-    customer_id = valor_customer_id,
-    store_id = valor_store_id,
-    first_name = 
---* Para eliminar un customer no hay problema
-ALTER TABLE payment DROP CONSTRAINT payment_customer_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
-ALTER TABLE rental DROP CONSTRAINT rental_customer_id_fkey;
-ALTER TABLE rental ADD CONSTRAINT rental_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+SET
+	customer_id = 602,
+	store_id = 2,
+	first_name = 'Nombre',
+	last_name = 'Apellido',
+	address_id = 602,
+	activebool = true,
+	last_update = CURRENT_DATE, 
+	active = 1
+WHERE customer_id = 601;
 
---* Para eliminar staff si hay problemas.... 
-BEGIN TRANSACTION;
-ALTER TABLE payment DROP CONSTRAINT payment_staff_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_staff_id_fkey FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE rental DROP CONSTRAINT rental_staff_id_key;
-ALTER TABLE rental ADD CONSTRAINT rental_staff_id_key FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE store DROP CONSTRAINT store_manager_staff_id_fkey;
-ALTER TABLE store ADD CONSTRAINT store_manager_staff_id_fkey FOREIGN KEY (store_id) REFERENCES store(store_id) ON DELETE CASCADE;
---! Lanza este error
---! ERROR:  null value in column "rental_id" of relation "payment" violates not-null constraint
---! DETAIL:  Failing row contains (18173, 516, 2, null, 4.99, 2007-02-14 21:23:39.996577).
---! CONTEXT:  SQL statement "UPDATE ONLY "public"."payment" SET "rental_id" = NULL WHERE $1 OPERATOR(pg_catalog.=) "rental_id""
---! SQL state: 23502
+-- Delete
+DELETE FROM customer WHERE customer_id = 602;
 
---* Para eliminar un actor no hay problema
-ALTER TABLE film_actor DROP CONSTRAINT film_actor_actor_id_fkey;
-ALTER TABLE film_actor ADD CONSTRAINT film_actor_actor_id_fkey FOREIGN KEY (actor_id) REFERENCES actor(actor_id) ON DELETE CASCADE;
+--* TABLA STAFF
 
+-- Insertar:
+INSERT INTO staff
+VALUES  
+    (3, 'Staffer', 'McStaff', 5, 'correo2', 1, true, 'staff', 'qwerty', CURRENT_DATE);
 
+-- Update
+UPDATE staff
+SET	
+	staff_id = 4,
+	first_name = 'Stuffo',
+	last_name = 'McStufy',
+	address_id = 6,
+	email = 'correo3',
+	store_id = 2,
+	active = false, 
+	username = 'stufo',
+	password = 'asdfghj'
+WHERE staff_id = 3;
+
+-- Delete
+DELETE FROM staff WHERE staff_id = 4;
+
+--* TABLA ACTOR
+
+-- Insertar
+INSERT INTO actor 
+VALUES
+	(201, 'Actor', 'McActor', CURRENT_DATE);
+
+-- Update
+UPDATE actor
+SET
+	actor_id = 202,
+	first_name = 'Bactor',
+	last_name = 'BaBactor'
+WHERE actor_id = 201;
+
+-- Delete
+DELETE FROM actor WHERE actor_id = 202;
 
 -- • Listar todas las “rental” con los datos del “customer” dado un año y mes.
 --* Retorna le fecha, nombre, apellido, email y pelicula 
@@ -47,6 +80,15 @@ SELECT to_char(rental_date, 'YYYY-MM'), customer.first_name, customer.last_name,
         INNER JOIN film ON inventory.film_id = film.film_id
     WHERE customer.first_name = 'Mary' AND customer.last_name = 'Smith' AND to_char(rental_date, 'YYYY-MM') = '2005-06';
 
+--* Esto hace lo mismo, pero genera la lista entera de clientes ordenados alfabéticamente.
+SELECT to_char(rental.rental_date, 'YYYY-MM'), customer.first_name, customer.last_name, email, title
+    FROM rental
+        INNER JOIN customer ON rental.customer_id = customer.customer_id 
+        INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+        INNER JOIN film ON inventory.film_id = film.film_id
+    WHERE to_char(rental_date, 'YYYY-MM') = '2005-06'
+    ORDER BY (customer.first_name, customer.last_name);
+
 -- • Listar Número, Fecha (payment_date) y Total (amount) de todas las “payment”.
 --* Decidí agruparlo por día, pero para agrupar por año o mes es trivial. Solo es neesario modificar 'YYYY-MM' ó 'YYYY' según sea el caso.
 SELECT to_char(payment_date, 'YYYY-MM-DD') AS "Fecha", count(*) AS "Cantidad", sum(amount) AS "Subtotal"  
@@ -56,11 +98,13 @@ ORDER BY "Fecha";
 
 
 -- • Listar todas las “film” del año 2006 que contengan un (rental_rate) mayor a 4.0.
+--* Relativamente trivial.
 SELECT title, release_year, rental_rate FROM film WHERE release_year = 2006 AND rental_rate > 4.0;
 
 
 -- 5. Realiza un Diccionario de datos que contenga el nombre de las tablas y columnas, si
 -- éstas pueden ser nulas, y su tipo de dato correspondiente.
+--* Lo visto en clases.
 SELECT
     t1.TABLE_NAME AS tabla_nombre,
     t1.COLUMN_NAME AS columna_nombre,
